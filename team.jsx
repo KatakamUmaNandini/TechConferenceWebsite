4cafaeb4f9a57442dac0edffc001554d339e73fc
KatakamUmaNandini <22MH1A05I5@acoe.edu.in>
2025-02-16 19:23:18 +0530

    Add files via upload

---
+++ team.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import "./team.css";
+const Team = ()=>{
+    return(
+        <>
+            <div className="maindiv">
+                <div className="teamheading">Meet the GDG Team</div>
+                <div className="teamintro">GDG ACET is more than just a tech community—We are here to create opportunities, bridge the gap between students and industry experts, and bring the best of technology right to your doorstep.</div>
+                <div className="teamlistdiv">
+                    <div className="membercard">
+                        <div className="upperpart"><div className="photodiv"></div></div>
+                        <h3>G.Sai Akhil</h3> 
+                        <div className="roles">Team Lead</div>
+                    </div>
+                    <div className="membercard">
+                        <div className="upperpart"><div className="photodiv"></div></div>
+                        <h3>G.Kedarinadh Sai Harsha</h3> 
+                        <div className="roles">Co-organizer</div>
+                    </div>
+                    <div className="membercard">
+                        <div className="upperpart"><div className="photodiv"></div></div>
+                        <h3>Shanmukh Sai Chowdary</h3> 
+                        <div className="roles">Cloud Head</div>
+                    </div>
+                    <div className="membercard">
+                        <div className="upperpart"><div className="photodiv"></div></div>
+                        <h3>Indra Narayan Yadav</h3> 
+                        <div className="roles">CyberSecurity Head</div>
+                    </div>
+                    <div className="membercard">
+                        <div className="upperpart"><div className="photodiv"></div></div>
+                        <h3>B. Akanksha</h3> 
+                        <div className="roles">SocialMedia Head</div>
+                    </div>
+                    <div className="membercard">
+                        <div className="upperpart"><div className="photodiv"></div></div>
+                        <h3>Prem</h3> 
+                        <div className="roles">Event Manager</div>
+                    </div>
+                    <div className="membercard">
+                        <div className="upperpart"><div className="photodiv"></div></div>
+                        <h3>S.Yasaswini</h3> 
+                        <div className="roles">Female Techie</div>
+                    </div>
+                    <div className="membercard">
+                        <div className="upperpart"><div className="photodiv"></div></div>
+                        <h3>K.Uma Nandini</h3> 
+                        <div className="roles">Female Techie</div>
+                    </div>
+                    <div className="membercard">
+                        <div className="upperpart"><div className="photodiv"></div></div>
+                        <h3>Harika</h3> 
+                        <div className="roles">Female Techie</div>
+                    </div>
+                    <div className="membercard">
+                        <div className="upperpart"><div className="photodiv"></div></div>
+                        <h3>Sushma</h3> 
+                        <div className="roles">Networking</div>
+                    </div>
+                    <div className="membercard">
+                        <div className="upperpart"><div className="photodiv"></div></div>
+                        <h3>Vasanthi</h3> 
+                        <div className="roles">Networking</div>
+                    </div>
+                    <div className="membercard">
+                        <div className="upperpart"><div className="photodiv"></div></div>
+                        <h3>Pranthi</h3> 
+                        <div className="roles">Social Media</div>
+                    </div>
+                    <div className="membercard">
+                        <div className="upperpart"><div className="photodiv"></div></div>
+                        <h3>B.Kumar Raj</h3> 
+                        <div className="roles">Designer</div>
+                    </div>
+                    <div className="membercard">
+                        <div className="upperpart"><div className="photodiv"></div></div>
+                        <h3>Nithin</h3> 
+                        <div className="roles">Designer</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+export default Team;
